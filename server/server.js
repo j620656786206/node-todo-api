13e3c59d7c57f351f1f2401cb683dc627fe7214b
j620656786206 <j620656786206@gmail.com>
2018-05-25 13:18:15 -0500

    79. Testing GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -48,7 +48,7 @@ app.get('/todos/:id', (req, res) => {
       // 400 - and send empty body back
   Todo.findById(id).then((todo) => {
     if(!todo) {
-      return res.status(400).send();
+      return res.status(404).send();
     }
 
     res.send({todo});
