c80db22ff5d8c56c9c60daaf77b8e826e89e1393
j620656786206 <j620656786206@gmail.com>
2018-05-25 12:00:50 -0500

    78. Getting an Individual Resource - GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectId} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -26,6 +27,33 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+// GET /todos/123456
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  // Valid id using isValid
+    // 404 - send back empty 
+  if(!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  // findById
+    // success
+      // if todo - send it back
+      // if no todo - send back 404 with empty body
+    // error
+      // 400 - and send empty body back
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(400).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
