f66870d0248df96ddd8efdb8f54cd72a0607954e
j620656786206 <j620656786206@gmail.com>
2018-05-29 16:33:09 -0500

    90. Generating Auth Tokens and Setting Headers

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
+const _         = require('lodash');
+const mongoose  = require('mongoose');
 const validator = require('validator');
-const jwt = require('jsonwebtoken');
-const _ = require('lodash');
+const jwt       = require('jsonwebtoken');
 
 var UserSchema = new mongoose.Schema({
   email: {
